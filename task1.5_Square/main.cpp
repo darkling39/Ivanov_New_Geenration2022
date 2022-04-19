bae13afde374673efd4a9968f913b5407eef6d44
Max <mafiozi1994w@gmail.com>
2022-04-19 18:59:40 +0300

    task1.5 is done by 1 commit because of its easiness

---
+++ task1.5_Square/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    short a, b, c;
+    float x1, x2, D;
+    cout << "Enter a, b, c: " << endl;
+    cin >> a >> b >> c;
+    D = pow(b, 2) - (4 * a * c);
+    if(D > 0)
+    {
+        x1 = (-b + sqrt(D)) / (2 * a);
+        x2 = -b - sqrt(D) / (2 * a);
+        cout << "x1 = " << x1 << endl
+             << "x2 = " << x2 << endl;
+    }
+    if (D == 0)
+    {
+        x1 = -b/2*a;
+        cout << "x = " << x1 << endl;
+    }
+    if(D < 0)
+        cout << "Null" << endl;
+    return 0;
+}
