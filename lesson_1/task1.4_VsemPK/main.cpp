a9a054fc4ef5747427cf753bc71b5efcae9d3133
Max <mafiozi1994w@gmail.com>
2022-04-20 15:53:46 +0300

    Created new folder(lesson1)

---
+++ lesson_1/task1.4_VsemPK/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    short mother, videocard, CPU, discount;
+    cout << "Motherboard price: ";
+    cin >> mother;
+    cout << "Videocard price: ";
+    cin >> videocard;
+    cout << "CPU price: ";
+    cin >> CPU;
+    cout << "Dicount: ";
+    cin >> discount;
+    cout << "Your PC will cost - " <<  (mother+videocard+CPU) - (((mother+videocard+CPU) *discount)/100) << "$";
+    return 0;
+}
