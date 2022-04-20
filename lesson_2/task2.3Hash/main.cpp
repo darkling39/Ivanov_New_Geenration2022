e73b1417923919965bc71ad8cd758e9d6e41b0fe
Max <mafiozi1994w@gmail.com>
2022-04-20 17:07:43 +0300

    task2.3(Project "HashCode" is done)

---
+++ lesson_2/task2.3Hash/main.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int code = 0, value;
+    cin >> value;
+    for (int cntVal = value; cntVal != 0; cntVal /= 10)
+    {
+        if (cntVal%10 == cntVal/10%10)
+            code += cntVal%10;
+        if (cntVal == value%10)
+            code += cntVal;
+    }
+    cout << code;
+    return 0;
+}
