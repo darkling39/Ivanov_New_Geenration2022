0b86c198da151129823e41497edc97ed2aba1339
Max <mafiozi1994w@gmail.com>
2022-04-20 16:02:58 +0300

    Task2.1(BorderSquare is done)

---
+++ lesson_2/task2.1_BorderSquare/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int sz;
+    cin >> sz;
+    for(int y = 0; y < sz; y++)
+    {
+        for(int x = 0; x < sz; x++)
+        {
+            if(y != 0 && y != sz - 1 && x != 0 && x != sz - 1 )
+            {
+                cout << " ";
+            }
+            else
+                cout << "*";
+        }
+        cout << endl;
+    }
+    return 0;
+}
