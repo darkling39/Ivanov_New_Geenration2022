c5efe74347475ae97e3c9406afdb4de7b8a2ebd5
Max <mafiozi1994w@gmail.com>
2022-04-26 16:58:42 +0300

    task3.3(Project StarRow) added interface

--- Lesson_3/task3.3StarRow/main.cpp
+++ Lesson_3/task3.3StarRow/main.cpp
@@ -6,6 +6,8 @@ int main()
 {
     int sz = 20, supp = 0;
     int pointer[sz];
+    cout << "Fill the array" << endl
+         << "(0 - end)"      << endl;
     for(int i = 0; i < sz; i++)
     {
         cin >> pointer[i];
