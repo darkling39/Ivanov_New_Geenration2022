799aec8c8b215c0f53111e3f926b0f532bfa687d
Max <mafiozi1994w@gmail.com>
2022-05-09 10:28:00 +0300

    task1.5(Project Square) fix: now creation of vars depends on if

--- lesson_1/task1.5_Square/main.cpp
+++ lesson_1/task1.5_Square/main.cpp
@@ -5,23 +5,21 @@ using namespace std;
 int main()
 {
     short a, b, c;
-    float x1, x2, D;
     cout << "Enter a, b, c: " << endl;
     cin >> a >> b >> c;
-    D = pow(b, 2) - (4 * a * c);
-    if(D > 0)
+    if((pow(b, 2) - (4 * a * c)) > 0)
     {
-        x1 = (-b + sqrt(D)) / (2 * a);
-        x2 = -b - sqrt(D) / (2 * a);
+        float x1 = (-b + sqrt(pow(b, 2) - (4 * a * c))) / (2 * a);
+        float x2 = -b - sqrt(pow(b, 2) - (4 * a * c)) / (2 * a);
         cout << "x1 = " << x1 << endl
              << "x2 = " << x2 << endl;
     }
-    if (D == 0)
+    if ((pow(b, 2) - (4 * a * c)) == 0)
     {
-        x1 = -b/2*a;
+        float x1 = -b/2*a;
         cout << "x = " << x1 << endl;
     }
-    if(D < 0)
+    if((pow(b, 2) - (4 * a * c)) < 0)
         cout << "Null";
     return 0;
 }
