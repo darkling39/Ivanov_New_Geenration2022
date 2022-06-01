cae88d8795343f7eec93d52602dd9e677d4e8afb
Max <mafiozi1994w@gmail.com>
2022-06-01 21:21:32 +0300

    task4.1(Project LargerString) fix: now only char arrays

--- lesson_4/task4.1LargerString/main.cpp
+++ lesson_4/task4.1LargerString/main.cpp
@@ -5,17 +5,34 @@ using namespace std;
 
 int main()
 {
-    string fir;
-    string sec;
+    char fir[256] = "";
+    char sec[256] = "";
+    short cnt1, cnt2;
     cout << "Enter first string:" << endl;
-    getline(cin, fir);
+    cin.getline(fir, 256);
     cout << "Enter second string:" << endl;
-    getline(cin, sec);
-    if(fir.length() < sec.length())
-        cout << "First" << " < " << "Second";
-    if(fir.length() > sec.length())
-        cout << "First" << " > " << "Second";
+    cin.getline(sec, 256);
+    for(int i = 0; i < 256; i++)
+    {
+        if(fir[i] != 0)
+            cnt1++;
+        else
+            break;
+
+    }
+    for(int i = 0; i < 256; i++)
+    {
+        if(sec[i] != 0)
+            cnt2++;
+        else
+            break;
+
+    }
+    if(cnt1 < cnt2)
+        cout << "First" << " < " << "Second" << endl;
+    else if(cnt1 > cnt2)
+        cout << "First" << " > " << "Second" << endl;
     else
-        cout << "First" << " = " << "Second";
+        cout << "First" << " = " << "Second" << endl;
     return 0;
 }
