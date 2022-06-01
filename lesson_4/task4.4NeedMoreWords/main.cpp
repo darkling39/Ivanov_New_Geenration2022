9e5c7de40911e9b10103dec451a4c0c1c1975b56
Max <mafiozi1994w@gmail.com>
2022-06-01 21:31:27 +0300

    task4.4(Project NeedMoreWords) fix: now only char arrays

--- lesson_4/task4.4NeedMoreWords/main.cpp
+++ lesson_4/task4.4NeedMoreWords/main.cpp
@@ -6,14 +6,16 @@ using namespace std;
 int main()
 {
     int cnt=0;
-    string print;
-    getline(cin, print);
-    for(int i = 0; i < print.length(); i++)
+    char print[256] = "";
+    cin.getline(print, 256);
+    for(int i = 0; i < 256; i++)
     {
         if( ((print[i] >= 97 && print[i] <= 122) || (print[i] >= 65 && print[i] <= 90)) && print[i-1] < 65 ||
             ((print[i] >= 97 && print[i] <= 122) && (print[i+1] >= 65 && print[i+1] <= 90))
           )
             cnt ++;
+        if(print[i] == 0)
+            break;
 
     }
     cout << cnt;
