aefd4339313d36d35721bbccde7286c7fb4e9023
Max <mafiozi1994w@gmail.com>
2022-06-16 18:04:33 +0300

    task4.4(NeedMoreWords) fix: now more simple

--- lesson_4/task4.4NeedMoreWords/main.cpp
+++ lesson_4/task4.4NeedMoreWords/main.cpp
@@ -7,16 +7,15 @@ int main()
 {
     int cnt=0;
     char print[256] = "";
+    cout << "Enter something: " << endl;
     cin.getline(print, 256);
     for(int i = 0; i < 256; i++)
     {
         if( ((print[i] >= 97 && print[i] <= 122) || (print[i] >= 65 && print[i] <= 90)) && print[i-1] < 65 ||
-            ((print[i] >= 97 && print[i] <= 122) && (print[i+1] >= 65 && print[i+1] <= 90))
-          )
+            ((print[i] >= 97 && print[i] <= 122) && (print[i+1] >= 65 && print[i+1] <= 90)) )
             cnt ++;
         if(print[i] == 0)
             break;
-
     }
     cout << cnt;
 }
